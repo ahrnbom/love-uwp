6e0f56a0e428d23bcc5b6bc0006859ae782ce70e
Martin Ahrnbom <Martin Ahrnbom>
2016-03-28 22:43:48 +0200

    Fixes for loading .love file

--- love/src/love.cpp
+++ love/src/love.cpp
@@ -52,10 +52,6 @@ extern "C"
 }
 #endif
 
-#ifdef LOVE_WINDOWS_UWP
-#include "../LöveFTW/lovefiletoopen.h"
-#endif
-
 #ifdef LOVE_WINDOWS
 extern "C"
 {
@@ -202,11 +198,13 @@ int main(int argc, char **argv)
 #endif // LOVE_LEGENDARY_APP_ARGV_HACK
 
 #ifdef LOVE_WINDOWS_UWP
-	// Since we don't have access to the actual argc and argv from SDL's main function, we'll just hack them inside here
-	argc = 2;
-	char* lovefile = lovefiletoopen::getCStr();
-	char* args[2] = { "love.exe", lovefile };
-	argv = args;
+	if (argc == 0) 
+	{
+		argc = 2;
+		char* lovefile = "game.love";
+		char* args[2] = { "love.exe", lovefile };
+		argv = args;
+	}
 #endif
 
 	if (strcmp(LOVE_VERSION_STRING, love_version()) != 0)

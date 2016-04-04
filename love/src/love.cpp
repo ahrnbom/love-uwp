5dfc9b3d0e74a4118e00ac4591df884beb54ac64
Martin Ahrnbom <Martin Ahrnbom>
2016-04-04 20:58:01 +0200

    Removed unnecessary code.

--- love/src/love.cpp
+++ love/src/love.cpp
@@ -197,16 +197,6 @@ int main(int argc, char **argv)
 	argv = hack_argv;
 #endif // LOVE_LEGENDARY_APP_ARGV_HACK
 
-#ifdef LOVE_WINDOWS_UWP
-	if (argc == 0) 
-	{
-		argc = 2;
-		char* lovefile = "game.love";
-		char* args[2] = { "love.exe", lovefile };
-		argv = args;
-	}
-#endif
-
 	if (strcmp(LOVE_VERSION_STRING, love_version()) != 0)
 	{
 		printf("Version mismatch detected!\nLOVE binary is version %s\n"
